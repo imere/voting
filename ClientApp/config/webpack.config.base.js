801e808bd0cc4f66b4d2118c4af1508e9b5508fa
imere <imere@qq.com>
2021-01-11 14:24:56 +0800

    fix(clientapp): bundle error

--- ClientApp/config/webpack.config.base.js
+++ ClientApp/config/webpack.config.base.js
@@ -141,42 +141,6 @@ const baseConfig = {
     new webpack.WatchIgnorePlugin({
       paths: [/\.d\.tsx?$/]
     }),
-    // new FriendlyErrorsWebpackPlugin(),
-    // new HardSourcePlugin({
-    //   // Either an absolute path or relative to webpack's options.context.
-    //   cacheDirectory: require("path").resolve(__dirname, "../node_modules/.cache/hard-source/[confighash]"),
-    //   // Either a string of object hash function given a webpack config.
-    //   configHash: function (webpackConfig) {
-    //     // node-object-hash on npm can be used to build this.
-    //     return require("node-object-hash")({ sort: false }).hash(webpackConfig);
-    //   },
-    //   // Either false, a string, an object, or a project hashing function.
-    //   environmentHash: {
-    //     root: process.cwd(),
-    //     directories: [],
-    //     files: [
-    //       "package-lock.json",
-    //       "yarn.lock"
-    //     ],
-    //   },
-    //   // An object.
-    //   info: {
-    //     // 'none' or 'test'.
-    //     mode: "none",
-    //     // 'debug', 'log', 'info', 'warn', or 'error'.
-    //     level: "error",
-    //   },
-    //   // Clean up large, old caches automatically.
-    //   cachePrune: {
-    //     // Caches younger than `maxAge` are not considered for deletion. They must
-    //     // be at least this (default: 2 days) old in milliseconds.
-    //     maxAge: 5 * 60 * 1000,
-    //     // All caches together must be larger than `sizeThreshold` before any
-    //     // caches will be deleted. Together they must be at least this
-    //     // (default: 50 MB) big in bytes.
-    //     sizeThreshold: 50 * 1024 * 1024
-    //   },
-    // }),
     new DuplicatePackageCheckerPlugin(),
     new StylelintPlugin({
       configFile: undefined,
